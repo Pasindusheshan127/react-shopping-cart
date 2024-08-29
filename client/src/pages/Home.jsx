32a3a08103946501a53931ca6978b0b9ef7ff1ef
pasindusheshan127 <pasinduhettiarachchi201@gmail.com>
2024-08-30 00:55:48 +0530

    create components

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,8 +1,17 @@
 import React from 'react'
+import { products } from '../data/products'
+import ProductCart from '../components/ProductCart'
 
 const home = () => {
   return (
-    <div>home</div>
+    <div>
+      <h1 className='text-3xl my-5'>List Products</h1>
+      <div className="grid lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-2 gap-5">
+        {products.map(product =>(
+          <ProductCart key='' data={product}/>
+        ))}
+      </div>
+    </div>
   )
 }
 
